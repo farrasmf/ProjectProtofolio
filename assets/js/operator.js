878684797d9cfc4475341b1cfbbfc39d189738e3
farrasmf <farrasmuhammadfurqon@gmail.com>
2023-08-14 22:21:05 +0700

    pernaikan conflict

--- assets/js/operator.js
+++ assets/js/operator.js
@@ -1,3 +1,4 @@
+<<<<<<< feature/blogs
 const students = ["student1", "student2", "student3"];
 let students2 = [];
 let student1;
@@ -8,3 +9,14 @@ students.forEach((item) => {
 });
 
 console.log(student1);
+=======
+let result = '';
+let i = 1;
+
+do {
+    i = i + 1;
+    result = result + i;
+} while (i<5);
+
+console.log(result);
+>>>>>>> main
