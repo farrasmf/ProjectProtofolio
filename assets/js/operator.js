f76f711af87e689ff019fc22f203a92775e31a5f
farrasmf <farrasmuhammadfurqon@gmail.com>
2023-08-15 12:11:07 +0700

    perbaikan conflict

--- assets/js/operator.js
+++ assets/js/operator.js
@@ -1,3 +1,14 @@
+const students = ["student1", "student2", "student3"];
+let students2 = [];
+let student1;
+students.forEach((item) => {
+    students2.push(item + " Ini Sekolah 2");
+    // harus memasukan argument seperti apa? nilanya student1 untuk variable student1
+    student1.push = ["student1"]
+});
+
+console.log(student1);
+=======
 let result = '';
 let i = 1;
 
